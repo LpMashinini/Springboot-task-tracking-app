f07512a72d27f6e9b4e37b5ba79eeca9a7e6f86a
LpMashinini <lebogangp84@gmail.com>
2026-06-10 16:22:22 +0200

    Created and defined Repository

---
+++ src/main/java/com/taskApp/taskApp/repository/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.taskApp.taskApp.repository;
+
+import com.taskApp.taskApp.domain.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+
+
+}
